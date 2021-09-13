e0d43dc5ae63c106e73dd7ff26ee89028465445c
Mário Gonçalves <mario.mc.goncalves@gmail.com>
2021-09-13 16:19:25 +0100

    Fix copy in popup page

--- src/pages/popup/App.tsx
+++ src/pages/popup/App.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 const App = (): JSX.Element => {
   return (
     <div>
-      <h1>Popup Page</h1>
       <p>This page will contain the future settings for WayfarerPlusUltra</p>
     </div>
   )
